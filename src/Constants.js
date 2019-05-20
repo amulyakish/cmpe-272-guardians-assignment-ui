780cae555d25a9872fbc354b8dfb0a11dff11f66
amulyakish <amulyakish@gmail.com>
2019-05-19 21:05:02 -0700

    Wrong commit

--- src/Constants.js
+++
@@ -1,8 +0,0 @@
-export const CONFIG = {
-    domain: 'assignment-walk.auth0.com',
-    clientId: 'reDXRyzcftLqoNzvmdmNuhmKnEV53Nqo',
-    audience: 'http://walk/api',
-    callbackUrl: 'http://localhost/callback',
-    logoutUrl : 'http://localhost/logout',
-    APIBaseUrl : 'http://localhost:8080'
-}
