3207c48fb977f371b144de6099e260c4f7479f72
amuk <amuk@cisco.com>
2019-05-19 21:09:00 -0700

    Committing UI files

--- src/Constants.js
+++ src/Constants.js
@@ -2,7 +2,7 @@ export const CONFIG = {
     domain: 'assignment-walk.auth0.com',
     clientId: 'reDXRyzcftLqoNzvmdmNuhmKnEV53Nqo',
     audience: 'http://walk/api',
-    callbackUrl: 'http://localhost/callback',
-    logoutUrl : 'http://localhost/logout',
+    callbackUrl: 'http://localhost:3000/callback',
+    logoutUrl : 'http://localhost:3000/logout',
     APIBaseUrl : 'http://localhost:8080'
 }
---
+++ src/components/ProductList.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import Product from "./Product";
+import Title from "./Title";
+import styled from "styled-components";
+import { ProductConsumer } from "../context";
+import {ProductContext} from '../context';
+import axios from 'axios';
+import toastr from 'toastr';
+import "toastr/build/toastr.min.css";
+// import "toastr/build/toastr.min.css";
+import { CONFIG } from '../Constants'
+
+
+export default class ProductList extends Component {
+  static contextType = ProductContext;
+  // state = {
+  //   products: storeProducts
+  // };
+
+  constructor(props){
+    super(props);
+    this.state = {
+      books: []
+    }
+  }
+
+  componentDidMount(){
+    let value = this.context;
+    if(this.props.auth.getAccessToken() != null && value.products.length === 0){
+      const headers = { 'Authorization': `Bearer ${this.props.auth.getAccessToken()}` }
+      axios.get(`${CONFIG.APIBaseUrl}/api/books`, { headers })
+      .then(res => {
+        const books = res.data;
+        books.forEach(function(e) {
+          e.img = 'img/product-1.png';
+          e.author = 'Mr. Author';
+        });
+        value.setProducts(books);
+        this.setState({books: books });
+      })
+    }
+    else{
+      window.location.reload();
+    }
+    console.log('ProductList componentDidMount', value);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <ProductWrapper className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+            <div className="row">
+              {
+                this.state.books.map(product => {
+                  return <Product key={product.bookId} product={product} />
+                })
+              }
+            </div>
+          </div>
+        </ProductWrapper>
+      </React.Fragment>
+    );
+  }
+}
+
+const ProductWrapper = styled.section``;
