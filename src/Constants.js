4496000262f5fa728d69e70fa70ccb587f4dbd3c
amuk <amuk@cisco.com>
2019-05-19 21:01:57 -0700

    initial commit

---
+++ src/Constants.js
@@ -0,0 +1,8 @@
+export const CONFIG = {
+    domain: 'assignment-walk.auth0.com',
+    clientId: 'reDXRyzcftLqoNzvmdmNuhmKnEV53Nqo',
+    audience: 'http://walk/api',
+    callbackUrl: 'http://localhost/callback',
+    logoutUrl : 'http://localhost/logout',
+    APIBaseUrl : 'http://localhost:8080'
+}
